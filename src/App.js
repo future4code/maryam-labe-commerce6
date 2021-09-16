a7101b8f9b2694bf9af4d7e208ed2df7ea4c26c4
Ytalo Cruz <ytalocrz@gmail.com>
2021-09-15 21:28:22 -0300

    Projeto E-commerce

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Inicio from './compoments/Inicio.js';
+import Filtrar from './compoments/Filtrar.js';
+import Fim from './compoments/Fim';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ class App extends React.Component { 
+  render() {
+    return (
+      <div>
+        <header> 
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdribbble.com%2Fshots%2F5926397-Ecommerce-Logo-Design&psig=AOvVaw3IqVCAUuvohjqCSHkh01bt&ust=1631837775030000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjzivWbgvMCFQAAAAAdAAAAABAD" />
+        <Inicio/>
+        </header>
+        <nav>
+         <Filtrar />
+        </nav>
+        <footer>
+          <Fim />
+        </footer>
+        
+      </div>
+    );
 }
-
-export default App;
+}
+export default App ;
---
+++ src/compoments/Fim.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+class Fim extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>Volte Sempre!</h2>
+            </div>
+        )
+    }
+}
+export default Fim;
