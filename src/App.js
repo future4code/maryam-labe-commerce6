4c55e22c281b0a74f0762dea21877dfffda081c6
Carina Santos Ferreira <carinasf14@gmail.com>
2021-09-15 21:29:54 -0300

    Estruturei os componentes produtos e carrinho

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Produtos from './components/Produtos';
+import Carrinho from './components/Carrinho';
 
-function App() {
+class App extends React.Component {
+  // state = {
+  //   cardProdutos: [
+  //       {
+  //       id: 1,
+  //       name: "Produto 1",
+  //       value: 100.0,
+  //       imageUrl: "https://picsum.photos/200/200",
+  //       },
+  //       {
+  //       id: 2,
+  //       name: "Produto 2",
+  //       value: 150.0,
+  //       imageUrl: "https://picsum.photos/200/200",
+  //       },
+  //       {
+  //       id: 1,
+  //       name: "Produto 3",
+  //       value: 200.0,
+  //       imageUrl: "https://picsum.photos/200/200",
+  //       },
+
+  //   ]};
+
+  render(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id ='div-root' >
+      <div>
+  
+       <div> 
+         <Produtos/> 
+         <cardProdutos/>
+       </div>
+       <main>
+         <img src ="https://picsum.photos/200/200" ></img>
+         <p>PRODUTO X</p>
+         <img src ="https://picsum.photos/200/200" ></img>
+         <p>PRODUTO X</p>
+         <img src ="https://picsum.photos/200/200" ></img>
+         <p>PRODUTO X</p>
+       </main>
+
+       <div> <Carrinho/> </div>
+
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
